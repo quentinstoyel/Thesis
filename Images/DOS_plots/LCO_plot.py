99b137453b32eb348087117fb96c91934d9b1736
Quentin Stoyel <quentin.stoyel@mail.mcgill.ca>
2018-10-25 17:05:49 -0400

    added some DOS stuff

---
+++ Images/DOS_plots/LCO_plot.py
@@ -0,0 +1,13 @@
+# coding: utf-8
+data = pd.read_csv('Li2CO3.dos1ev', delim_whitespace=True, skiprows=3, names = ['E','tot','Li','C','O1','O2'])
+data.describe()
+data['O1'] = data['O1']+data['O2']
+for i in np.arange(4):
+    plt.plot(data['E'],data[data.keys()[i+1]])
+plt.axvline(x=0) #Fermi energy
+plt.title("DOS of Li2CO3")
+plt.xlim([-5,15])
+plt.xlabel("Energy (eV)")
+plt.ylabel("DOS (States/eV cell")
+    
+plt.show()
